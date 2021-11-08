3196a985d536e33c54dbd29f59f688bcf175d47d
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-08 18:39:26 +0100

    Create OpenLibraryAPI.java

---
+++ Scanner/app/src/main/java/com/example/scanner/OpenLibraryAPI.java
@@ -0,0 +1,11 @@
+package com.example.scanner;
+
+public class OpenLibraryAPI {
+    String api = "http://openlibrary.org/search.json?q=";
+    String isbn;
+
+    public OpenLibraryAPI(){
+        
+    }
+
+}
