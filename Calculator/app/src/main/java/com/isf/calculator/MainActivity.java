d520326737425cd2ea60066a7f60e8f02a173fea
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-20 17:10:35 +0100

    adding the updateText to all the button methods and fixinf minusBTN and plusBTN methods

--- Calculator/app/src/main/java/com/isf/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/isf/calculator/MainActivity.java
@@ -28,27 +28,76 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void updateText(String strToAdd){
+        String oldStrng = display.getText().toString();
+        //saving cursor user position so that he/she doesnt start typing at the beginning of the string
+        int cursorPos = display.getSelectionStart();
+        String leftString = oldStrng.substring(0,cursorPos);
+        String rightString = oldStrng.substring(cursorPos);
+        display.setText(String.format("%s%s%s",leftString,strToAdd,rightString));
+    }
 
-    public void zeroBTN(View view){}
-    public void oneBTN(View view){}
-    public void twoBTN(View view){}
-    public void threeBTN(View view){}
-    public void fourBTN(View view){}
-    public void fiveBTN(View view){}
-    public void sixBTN(View view){}
-    public void sevenBTN(View view){}
-    public void eightBTN(View view){}
-    public void nineBTN(View view){}
-    public void clearBTN(View view){}
-    public void exponentBTN(View view){}
-    public void parenthesesBTN(View view){}
-    public void divideBTN(View view){}
-    public void multiplyBTN(View view){}
-    public void minusBTN(View view){}
-    public void addBTN(View view){}
-    public void subtractBTN(View view){}
-    public void plusMinusBTN(View view){}
-    public void pointBTN(View view){}
-    public void equalsBTN(View view){}
-    public void backspaceBTN(View view){}
+    public void zeroBTN(View view){
+        updateText("@string/zero");
+    }
+    public void oneBTN(View view){
+        updateText("@string/one");
+    }
+    public void twoBTN(View view){
+        updateText("@string/two");
+    }
+    public void threeBTN(View view){
+        updateText("@string/three");
+    }
+    public void fourBTN(View view){
+        updateText("@string/four");
+    }
+    public void fiveBTN(View view){
+        updateText("@string/five");
+    }
+    public void sixBTN(View view){
+        updateText("@string/six");
+    }
+    public void sevenBTN(View view){
+        updateText("@string/seven");
+    }
+    public void eightBTN(View view){
+        updateText("@string/eight");
+    }
+    public void nineBTN(View view){
+        updateText("@string/nine");
+    }
+    public void clearBTN(View view){
+        updateText("@string/clear");
+    }
+    public void exponentBTN(View view){
+        updateText("@string/exponent");
+    }
+    public void parenthesesBTN(View view){
+        updateText("@string/parenthese");
+    }
+    public void divideBTN(View view){
+        updateText("@string/divide");
+    }
+    public void multiplyBTN(View view){
+        updateText("@string/multiply");
+    }
+    public void addBTN(View view){
+        updateText("@string/add");
+    }
+    public void subtractBTN(View view){
+        updateText("@string/subtract");
+    }
+    public void plusMinusBTN(View view){
+        updateText("@string/plusMinus");
+    }
+    public void pointBTN(View view){
+        updateText("@string/point");
+    }
+    public void equalsBTN(View view){
+        updateText("@string/equals");
+    }
+    public void backspaceBTN(View view){
+        updateText("@string/backspace");
+    }
 }
