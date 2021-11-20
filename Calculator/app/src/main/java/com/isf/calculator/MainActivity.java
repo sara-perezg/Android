04d2cf8c88f01347467e9d09e2227941792e8a74
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2021-11-20 17:43:11 +0100

    getting value from string with context.getR... in each button method. I was using the id instead. Bad choise!

--- Calculator/app/src/main/java/com/isf/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/isf/calculator/MainActivity.java
@@ -29,75 +29,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateText(String strToAdd){
+
         String oldStrng = display.getText().toString();
         //saving cursor user position so that he/she doesnt start typing at the beginning of the string
         int cursorPos = display.getSelectionStart();
         String leftString = oldStrng.substring(0,cursorPos);
         String rightString = oldStrng.substring(cursorPos);
         display.setText(String.format("%s%s%s",leftString,strToAdd,rightString));
+//        display
     }
 
     public void zeroBTN(View view){
-        updateText("@string/zero");
+        updateText(this.getResources().getString(R.string.zero));
     }
     public void oneBTN(View view){
-        updateText("@string/one");
+        updateText(this.getResources().getString(R.string.one));
     }
     public void twoBTN(View view){
-        updateText("@string/two");
+        updateText(this.getResources().getString(R.string.two));
     }
     public void threeBTN(View view){
-        updateText("@string/three");
+        updateText(this.getResources().getString(R.string.three));
     }
     public void fourBTN(View view){
-        updateText("@string/four");
+        updateText(this.getResources().getString(R.string.four));
     }
     public void fiveBTN(View view){
-        updateText("@string/five");
+        updateText(this.getResources().getString(R.string.five));
     }
     public void sixBTN(View view){
-        updateText("@string/six");
+        updateText(this.getResources().getString(R.string.six));
     }
     public void sevenBTN(View view){
-        updateText("@string/seven");
+        updateText(this.getResources().getString(R.string.seven));
     }
     public void eightBTN(View view){
-        updateText("@string/eight");
+        updateText(this.getResources().getString(R.string.eight));
     }
     public void nineBTN(View view){
-        updateText("@string/nine");
-    }
-    public void clearBTN(View view){
-        updateText("@string/clear");
+        updateText(this.getResources().getString(R.string.nine));
     }
     public void exponentBTN(View view){
-        updateText("@string/exponent");
+        updateText(this.getResources().getString(R.string.exponent));
     }
     public void parenthesesBTN(View view){
-        updateText("@string/parenthese");
+        updateText(this.getResources().getString(R.string.parentheses));
     }
     public void divideBTN(View view){
-        updateText("@string/divide");
+        updateText(this.getResources().getString(R.string.divide));
     }
     public void multiplyBTN(View view){
-        updateText("@string/multiply");
+        updateText(this.getResources().getString(R.string.multiply));
     }
     public void addBTN(View view){
-        updateText("@string/add");
+        updateText(this.getResources().getString(R.string.add));
     }
     public void subtractBTN(View view){
-        updateText("@string/subtract");
+        updateText(this.getResources().getString(R.string.subtract));
     }
     public void plusMinusBTN(View view){
-        updateText("@string/plusMinus");
+        updateText(this.getResources().getString(R.string.plusMinus));
     }
     public void pointBTN(View view){
-        updateText("@string/point");
+        updateText(this.getResources().getString(R.string.point));
+    }
+    public void clearBTN(View view){
+        display.setText("");
     }
     public void equalsBTN(View view){
-        updateText("@string/equals");
     }
     public void backspaceBTN(View view){
-        updateText("@string/backspace");
     }
 }
