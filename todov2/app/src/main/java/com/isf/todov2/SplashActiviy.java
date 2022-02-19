85511ad0f4908c1a27a9e98a3788c5f66e9c6090
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-02-19 13:05:06 +0100

    dealying launcher

--- todov2/app/src/main/java/com/isf/todov2/SplashActiviy.java
+++ todov2/app/src/main/java/com/isf/todov2/SplashActiviy.java
@@ -2,7 +2,9 @@ package com.isf.todov2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActiviy extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashActiviy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_activiy);
+        getSupportActionBar().hide(); // hiding the activity
+
+        final Intent intent = new Intent(SplashActiviy.this, MainActivity.class); //launchin my main activity
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },1000); // this created a delay when launching the mainactivity
     }
 }
