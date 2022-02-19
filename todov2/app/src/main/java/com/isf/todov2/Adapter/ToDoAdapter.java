8a92f9dc7d41e4fb452f204751993717420f8e90
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-02-19 16:09:57 +0100

    DatabaseHandler finished

---
+++ todov2/app/src/main/java/com/isf/todov2/Adapter/ToDoAdapter.java
@@ -0,0 +1,67 @@
+package com.isf.todov2.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.isf.todov2.MainActivity;
+import com.isf.todov2.Model.ToDoModel;
+import com.isf.todov2.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    /**
+     * The item in which we are going to be working on Form the holder I get the position adn we search it in the todoList
+     * we get the text form the task and set it as well as the checked status
+     * @param holder
+     * @param position
+     */
+    public void onBindViewHolder(ViewHolder holder,int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    private boolean toBoolean(int n){
+        return n != 0; // si n es distinto de 0, devuelve true (1) si n es 0 devuelve false
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+
+    }
+}
--- todov2/app/src/main/java/com/isf/todov2/MainActivity.java
+++ todov2/app/src/main/java/com/isf/todov2/MainActivity.java
@@ -6,14 +6,39 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.isf.todov2.Adapter.ToDoAdapter;
+import com.isf.todov2.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager((new LinearLayoutManager(this))); // defining the recycler view as linear manager
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        taskList = new ArrayList<>();
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
     }
 }
