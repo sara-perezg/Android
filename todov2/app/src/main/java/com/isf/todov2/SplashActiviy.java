e955865cc4233b924afded6f91d3044630d0d3b0
Sara pg <54843362+sara-perezg@users.noreply.github.com>
2022-02-19 12:56:43 +0100

    dependencies, layout of task and splashactivity

---
+++ todov2/app/src/main/java/com/isf/todov2/SplashActiviy.java
@@ -0,0 +1,14 @@
+package com.isf.todov2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activiy);
+    }
+}
